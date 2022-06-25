0ea5b25a06e0fc7cc8258a66f1715608b9c8aac9
Saske <Saske@DESKTOP-1QV8TE3>
2022-06-25 23:11:01 +0300

    Добавьте файлы проекта.

---
+++ zadanie 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
